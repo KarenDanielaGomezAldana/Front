34b04c83ac38c89801725f580b758928ef92bb14
Karen Daniela Gómez Aldana <63369380+KarenDanielaGomezAldana@users.noreply.github.com>
2022-05-02 17:46:43 -0500

    Recuperatorio

---
+++ src/componentes/Item.js
@@ -0,0 +1,11 @@
+// CONSIGNAS EN ITEM:  🚩
+// 1- crear un <li> que tenga su click preparado para ejecutar el metodo heredado para seleccionar la plataforma como favorita
+// 2- Presentar el nombre de la plataforma que le llega como prop
+
+export default function Item({plataforma, handleFavorita}) {
+  return (
+    <li onClick={() => handleFavorita(plataforma)}>
+      {plataforma}
+    </li>
+  );
+}
